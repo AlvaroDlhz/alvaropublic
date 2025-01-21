8834573c8e4e43afe74fd1479b209ddce529a9ce
Alvaro De la hoz <alvarodlz1999@gmail.com>
2025-01-20 23:42:56 -0500

    a

---
+++ js/visuals.js
@@ -0,0 +1,17 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+    const h2 = document.querySelector('.glassmorfism-title');
+
+    document.addEventListener('mousemove', (event) => {
+        const { clientX, clientY } = event;
+        const { left, top, width, height } = h2.getBoundingClientRect();
+        const h2CenterX = left + width / 2;
+        const h2CenterY = top + height / 2;
+
+        const deltaX = clientX - h2CenterX;
+        const deltaY = clientY - h2CenterY;
+
+        const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+        h2.style.transform = `rotate(${angle}deg)`;
+    });
+});*/
