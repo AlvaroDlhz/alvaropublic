9f2470046a8f1ed287909893338a5f44ba3f25f9
Alvaro De la hoz <alvarodlz1999@gmail.com>
2025-05-10 13:25:49 -0500

    comencé a trabajar en el envío automático de emails desde la página

--- js/visuals.js
+++ js/visuals.js
@@ -26,3 +26,16 @@
       track.appendChild(clone);
     });
   });
+
+
+
+
+  //funcion para envío de emails con emailjs
+  function sendEmail() {
+    let params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    };
+    emailjs.send("service_81xkq0a", "template_45ha295", params).then(alert("Email sent successfully!"))
+}
